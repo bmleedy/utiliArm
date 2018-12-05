bcc137e231663a1de52385c04a1add543ab12932
Brett <bmleedy@gmail.com>
2018-12-04 21:39:38 -0800

    Added Servo submodule to sweep code

--- main/blink.cpp
+++ main/blink.cpp
@@ -9,16 +9,33 @@
 #include "driver/gpio.h"
 #include "sdkconfig.h"
 
+#include "servoControl.h"
+
 /* Can run 'make menuconfig' to choose the GPIO to blink,
    or you can edit the following line and set a number here.
 */
-#define STEPPER_STEP_PIN 27
-#define STEPPER_DIRECTION_PIN 26
+#define STEPPER_STEP_PIN GPIO_NUM_27
+#define STEPPER_DIRECTION_PIN GPIO_NUM_26
 #define STEP_PERIOD_MS 25
 #define STEPS_TO_SWEEP 400
 
+#define SERVO_OUTPUT_PIN GPIO_NUM_5
+#define SERVO_SWEEP_DEGREES 30
+
 void blink_task(void *pvParameter)
 {
+  // Configure the GPIO pin for the servo
+  servoControl myServo;
+  myServo.attach(SERVO_OUTPUT_PIN);
+  //Defaults: myServo.attach(pin, 400, 2600, LEDC_CHANNEL_0, LEDC_TIMER0);
+  // to use more servo set a valid ledc channel and timer
+  myServo.write(0);  // sero out the servo
+
+
+
+
+
+  
   /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
      muxed to GPIO on reset already, but some default to other
      functions and need to be switched to GPIO. Consult the
@@ -28,7 +45,7 @@ void blink_task(void *pvParameter)
 
   //initialize state for the step pin and the direction
   bool direction_state = false;
-
+  int servo_command = SERVO_SWEEP_DEGREES / 2; //degrees
 
   // Select pad as a gpio function from IOMUX
   gpio_pad_select_gpio(STEPPER_STEP_PIN);
@@ -46,6 +63,9 @@ void blink_task(void *pvParameter)
     // Set direction pin
     gpio_set_level(STEPPER_DIRECTION_PIN, direction_state);
 
+    // Command the servo
+    myServo.write(servo_command);
+    
     // Step STEPS_TO_SWEEP times
     for( int i=0; i < STEPS_TO_SWEEP; i++){
       //go high for 1 ms
@@ -59,11 +79,11 @@ void blink_task(void *pvParameter)
 
     // Reverse direction state
     direction_state = !direction_state;
-
+    servo_command = -servo_command;
   }
 }
 
-void app_main()
+extern "C" void app_main()
 {
     xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
 }
