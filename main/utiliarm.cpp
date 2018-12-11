58c147c57310a44bd64664a6f7d623c2023ec034
Brett <bmleedy@gmail.com>
2018-12-10 22:45:23 -0800

    working on wifi does not find AP

--- main/utiliarm.cpp
+++ main/utiliarm.cpp
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include <tcpip_adapter.h>
 #include "driver/gpio.h"
 #include "sdkconfig.h"
 
@@ -14,6 +15,9 @@
 #include "esp_event.h"
 #include "esp_event_loop.h"
 #include "nvs_flash.h"
+
+#include "lwip/inet.h"
+#include "lwip/ip4_addr.h"
 
 #include "string.h"
 
@@ -31,67 +35,86 @@
 #define SERVO_SWEEP_DEGREES 100
 #define SERVO_OFFSET 90
 
+ip_addr_t ip_Addr;
+ip4_addr_t ip;
+ip4_addr_t gw;
+ip4_addr_t msk;
+bool bConnected = false;
+bool bDNSFound = false;
+
 esp_err_t event_handler(void *ctx, system_event_t *event)
 {
+	printf("event_handler: received event [%X]\n", event->event_id);
+
+    switch( event->event_id ){
+    	case SYSTEM_EVENT_STA_GOT_IP :
+    		/*
+			ip = event->event_info.got_ip.ip_info.ip;
+			gw = event->event_info.got_ip.ip_info.gw;
+			msk = event->event_info.got_ip.ip_info.netmask;
+			bConnected = true;
+
+			printf("Got IP: %s\n", inet_ntoa( ip ) );
+			printf("Net mask: %s\n", inet_ntoa( msk ) );
+			printf("Gateway: %s\n", inet_ntoa( gw ) );
+			*/
+			break;
+    	case SYSTEM_EVENT_STA_START :
+    		printf("Station start event.\n");
+    		esp_wifi_connect();
+    		break;
+    	case SYSTEM_EVENT_STA_CONNECTED :
+    		printf("Station Connected!\n");
+    		break;
+    	case SYSTEM_EVENT_STA_DISCONNECTED :
+    		printf("Station Disconnected!\n");
+    		esp_wifi_connect();
+    		//xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
+    		break;
+    	default:
+    		printf("event_handler: event %d", event->event_id);
+    }
+
     return ESP_OK;
 }
 
+
 void initialize_wifi()
 {
-  nvs_flash_init();
-  tcpip_adapter_init();
-  ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
-  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-  ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-  ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
-  ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
-  //create station config
-  wifi_config_t sta_config;
+	printf("wifi flash init...\n");
+	nvs_flash_init();
+	printf("wifi adapter init...\n");
+	tcpip_adapter_init();
+	printf("wifi event loop init\n");
+	ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+	printf("wifi init\n");
+	ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+	printf("wifi set storage\n");
+	ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+	printf("wifi set mode\n");
+	ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+	//create station config
+	wifi_config_t sta_config;
 
-  strncpy((char *)sta_config.sta.ssid,CONFIG_ESP_WIFI_SSID,32);
-  strncpy((char *)sta_config.sta.password,CONFIG_ESP_WIFI_PASSWORD,32);
-  sta_config.sta.bssid_set = false;
-  ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
-  ESP_ERROR_CHECK( esp_wifi_start() );
-  ESP_ERROR_CHECK( esp_wifi_connect() );
+	strncpy((char *)sta_config.sta.ssid,CONFIG_ESP_WIFI_SSID,32);
+	strncpy((char *)sta_config.sta.password,CONFIG_ESP_WIFI_PASSWORD,32);
+	sta_config.sta.bssid_set = false;
+	printf("wifi set config\n");
+	ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
+	printf("wifi start\n");
+	ESP_ERROR_CHECK( esp_wifi_start() );
+	printf("wifi connect\n");
+	ESP_ERROR_CHECK( esp_wifi_connect() );
+
 }
 
-
-void print_ip_address(){    
-  uint32_t ulIPAddress, ulNetMask, ulGatewayAddress, ulDNSServerAddress;
-  int8_t cBuffer[ 16 ];
-  
-  FreeRTOS_GetAddressConfiguration( &ulIPAddress,
-				    &ulNetMask,
-				    &ulGatewayAddress,
-				    &ulDNSServerAddress );
-  
-  /* Convert the IP address to a string then print it out. */
-  FreeRTOS_inet_ntoa( ulIPAddress, cBuffer );
-  printf( "IP Address: %s\r\n", cBuffer );
-  
-  /* Convert the net mask to a string then print it out. */
-  FreeRTOS_inet_ntoa( ulNetMask, cBuffer );
-  printf( "Subnet Mask: %s\r\n", cBuffer );
-  
-  /* Convert the IP address of the gateway to a string then print it out. */
-  FreeRTOS_inet_ntoa( ulGatewayAddress, cBuffer );
-  printf( "Gateway IP Address: %s\r\n", cBuffer );
-  
-  /* Convert the IP address of the DNS server to a string then print it out. */
-  FreeRTOS_inet_ntoa( ulDNSServerAddress, cBuffer );
-  printf( "DNS server IP Address: %s\r\n", cBuffer );
-}
 
 
 
 
 void motion_test_task(void *pvParameter)
 {
-
-  // initialize network
-  initialize_wifi();
-  network_config my_netconfig;
 
   // Configure the GPIO pin for the servo
   servoControl myServo;
@@ -117,9 +140,11 @@ void motion_test_task(void *pvParameter)
     //todo: crash or fail to build if STEP_PERIOD_MS is <= 1.
 
     // Set direction pin
+	printf("level set\n");
     gpio_set_level(STEPPER_DIRECTION_PIN, direction_state);
 
     // Command the servo
+    printf("servo write\n");
     myServo.write(servo_command+SERVO_OFFSET);
     
     // Step STEPS_TO_SWEEP times
@@ -138,9 +163,6 @@ void motion_test_task(void *pvParameter)
     servo_command = -servo_command;
     printf("Reversing Servo Direction........%d\n",servo_command);
 
-    // Print my IP Address
-    print_ip_address();
-
   }
 }
 
@@ -148,5 +170,8 @@ void motion_test_task(void *pvParameter)
 
 extern "C" void app_main()
 {
-    xTaskCreate(&motion_test_task, "motion_test", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
+	printf("initializing WiFi..\n");
+	initialize_wifi();
+
+    //xTaskCreate(&motion_test_task, "motion_test", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
 }
